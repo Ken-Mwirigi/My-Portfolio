b7e6270551b269d8b88511f958189a8ffa72c69d
Ken-Mwirigi <kamaukennedy362@gmail.com>
2025-02-21 22:43:53 +0300

    modify settings

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'Portfolio.urls'
