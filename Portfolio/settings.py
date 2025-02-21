d17fc7f4150d239330a996078d52444f0575b255
Ken-Mwirigi <kamaukennedy362@gmail.com>
2025-02-21 15:25:20 +0300

    Email sending

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -30,10 +30,10 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='kenmwirigi7933@gmail.com'
-EMAIL_HOST_PASSWORD='wxpo evjg ysby jayb'
-EMAIL_PORT = 587
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
